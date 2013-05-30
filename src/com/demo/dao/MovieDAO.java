7426bb57849e8d6246c11a97cf938ad7bd7e9b8b
dotatata <dotatata@163.com>
2013-05-30 17:26:16 +0800

    dotatata
    aaaaaa

---
+++ src/com/demo/dao/MovieDAO.java
@@ -0,0 +1,10 @@
+package com.demo.dao;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MovieDAO extends BaseHibernateDAO {
+	private static final Log log = LogFactory.getLog(UserDAO.class);
+
+	public static final String NAME = "name";
+}
