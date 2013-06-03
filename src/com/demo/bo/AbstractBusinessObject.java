0ee5f588a73e3d475952077aecff7423f6dd7641
noachianark <noachianark@gmail.com>
2013-06-03 13:59:05 +0800

    增加模型基类的一次性setProperty方法，传入servlet中的requestParams即可。

--- src/com/demo/bo/AbstractBusinessObject.java
+++ src/com/demo/bo/AbstractBusinessObject.java
@@ -3,10 +3,42 @@
  */
 package com.demo.bo;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.beanutils.BeanUtils;
+
 /**
  * @author ryan
  * 
  */
 public class AbstractBusinessObject {
+	
+	public AbstractBusinessObject setProperty(Map<String, Object> params){
+
+			Iterator<Entry<String, Object>> iter = params.entrySet().iterator();
+			while (iter.hasNext()) {
+			    Map.Entry entry = (Map.Entry) iter.next();
+			    try {
+					BeanUtils.setProperty(this, (String) entry.getKey(), entry.getValue());
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} 
+		
+		return this;
+	}
+	
+	
 
 }
---
+++ test/bo/AbstractBusinessObjectTestCase.java
@@ -0,0 +1,31 @@
+package bo;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.demo.DemoException;
+import com.demo.bo.User;
+
+import junit.framework.TestCase;
+
+
+public class AbstractBusinessObjectTestCase extends TestCase {
+
+	  @Test
+	  public void testInitUser() throws DemoException {
+
+			  //UserDAO userDao=(UserDAO)DAOFactory.getDAO("UserDAO");			  
+			 
+			  Map<String, Object> params = new HashMap<String, Object>();
+			  
+			  params.put("username", "lalala");
+			  params.put("password", "loolo");
+			  params.put("meiyou", "多余的参数");
+			  
+			  User user = new User();
+			  user.setProperty(params);
+			  assertNotNull(user.getUsername());
+			  assertNotNull(user.getPassword());
+	  }
+}
